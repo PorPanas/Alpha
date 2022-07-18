c4475ab40bc624d987801bf0885475ad4561a78d
HUAWEI <HUAWEI@LAPTOP-56AQNJFN>
2022-07-18 19:44:46 +0700

    Test GIT

---
+++ Beta1/src/main/java/com/besix/beta1/starter/Beta1Application.java
@@ -0,0 +1,13 @@
+package com.besix.beta1.starter;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Beta1Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Beta1Application.class, args);
+	}
+
+}
---
+++ Beta1/src/test/java/com/besix/beta1/starter/Beta1ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.besix.beta1.starter;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Beta1ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
